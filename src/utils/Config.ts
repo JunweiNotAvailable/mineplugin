2796968c69abe54d65e6ec741831510111fb518c
Junwei <wertycxx@gmail.com>
2024-02-13 14:05:49 +0800

    moved url to .env

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,6 +1,6 @@
 export const config = {
   api: {
-    s3: "https://3tdbs9e5ye.execute-api.ap-northeast-1.amazonaws.com/production",
-    codeBuild: 'https://pgujbjex83.execute-api.ap-northeast-1.amazonaws.com/prod',
+    s3: process.env.REACT_APP_S3_API_URL,
+    codeBuild: process.env.REACT_APP_CODE_BUILD_API_URL,
   }
 };
