020beb1169f3e079d445aac4b59e70283e939fc2
Junwei <wertycxx@gmail.com>
2024-02-17 10:14:49 +0800

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import EditPage from './pages/edit/EditPage';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import Plugin from './pages/plugin/Plugin';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
-      <EditPage />
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/:username' element={<Profile />} />
+          <Route path='/:username/:plugin_id' element={<Plugin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/plugin/Plugin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Plugin = () => {
+  return (
+    <div>Plugin</div>
+  )
+}
+
+export default Plugin
