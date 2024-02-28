5d08a8a952b81e9b9cac692e908774941af2f3ea
Junwei <wertycxx@gmail.com>
2024-02-28 21:52:14 +0800

    added simple landing page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Logo from "../asset/svgs/Logo"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-gray-700 flex justify-center text-gray-300'>
+      <div className="flex justify-between items-center py-4 px-8 w-full">
+        <div className="flex justify-center items-center font-light py-2">
+          <i className="fa-regular fa-copyright text-xs" />
+          <div className="text-sm ml-2">2024 MinePlugin</div>
+        </div>
+        <div className="w-6 mr-8">
+          <Logo />  
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/pages/home/Landing.tsx
+++ src/pages/home/Landing.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Footer from '../../components/Footer'
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion';
@@ -11,8 +10,12 @@ const Landing = () => {
       {/* hero */}
       <InView>
         {({ ref, inView }) => <div className='flex-1 relative' ref={ref}>
-          <div style={{ width: '100%', aspectRatio: '2/1', background: '#0004' }} />
-          <motion.div className='rounded-xl absolute top-1/2 -translate-y-1/2 p-8 max-w-xl' style={{ background: '#fff8' }} initial={{ opacity: 0, left: 32 }} animate={{ left: inView ? 64 : 32, opacity: inView ? 1 : 0 }} transition={{ duration: .5, ease: 'linear', delay: .3 }}>
+          <div style={{ width: '100%', aspectRatio: '2/1' }}>
+            <video loop autoPlay muted>
+              <source src={require('./river.mp4')} />
+            </video>
+          </div>
+          <motion.div className='rounded-xl absolute top-1/2 -translate-y-1/2 p-8 max-w-xl' style={{ background: '#fffa' }} initial={{ opacity: 0, left: 32 }} animate={{ left: inView ? 64 : 32, opacity: inView ? 1 : 0 }} transition={{ duration: .5, ease: 'linear', delay: .3 }}>
             <div className='font-bold text-4xl'>Build Minecraft plugins in anywhere, at anytime</div>
             <div className='mt-4'>MinePlugin is a free tool to code, build and download Minecraft plugins all in one place.</div>
             <button className='main-button mt-8 text-lg py-1 px-8' onClick={() => navigate('/login')}>Get started</button>
